49352b5d3b473e3909e19a6cbd5f85c28559e352
L-Ward <noreply>
2018-08-12 17:52:47 -0700

    API routes created to get drinks and ingredients

--- controllers/html_controller.js
+++ controllers/html_controller.js
@@ -1,6 +1,7 @@
 module.exports = function(app) {
-
-    app.get('/', function(req, res) {
+    
+    // Simple get route to see handlebars working
+    app.get("/", function(req, res) {
         res.render('index');
     });
 };
