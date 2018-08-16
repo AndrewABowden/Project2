7dc57201806ed5516fdda4cd3d70a765deef3d23
mattmory <matt.mory@gmail.com>
2018-08-16 09:59:19 -0700

    Controller Push

--- controllers/ingredient_controller.js
+++ controllers/ingredient_controller.js
@@ -21,4 +21,33 @@ module.exports = function (app) {
                 res.json(dbIngredients);
             });
     });
+
+    // Route for adding an ingredient
+    app.post("/api/ingredients", function (req, res) {
+        //Check to see if the ingredient exists
+        db.ingredients.findOne({
+            where: { name: req.body.name },
+        }).then(function (ingredient) {
+            if(ingredient!=null)
+            {
+                res.status(200).send("Ingredient exists.");
+            }
+            else
+            {
+                var desc = req.body.description;
+                if(desc === null || desc ==="")
+                {
+                    desc = null
+                }
+                db.ingredients.create({
+                    name: req.body.name,
+                    description: desc
+                }).then(function () {
+                    res.status(200).end();
+                })
+            }
+        }).catch(function (err) {
+            throw err;
+        })
+    })
 };
