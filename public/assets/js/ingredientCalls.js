21b570e0abdd7d105f87c851067bc3c09c07fdf4
L-Ward <noreply>
2018-08-15 20:54:35 -0700

    delete buttons working and info cards populated

--- public/assets/js/ingredientCalls.js
+++ public/assets/js/ingredientCalls.js
@@ -1,6 +1,20 @@
 var ingredientsNameArray = [];
 var ingredientsObjArray = [];
 
+// Delete ingedient from temporary ingredient list
+function deleteBtn() {
+    var ingredientName = ($(this).attr("value"));
+    var indexName = ingredientsNameArray.indexOf(ingredientName);
+    var foundObj = ingredientsObjArray.find((element) => {
+        return element.name == ingredientName
+    });
+    var indexObj = (ingredientsObjArray.indexOf(foundObj));
+    ingredientsNameArray.splice(indexName, 1);
+    ingredientsObjArray.splice(indexObj, 1);
+    $(this).parent().remove();
+    drinksDisplay();
+};
+
 // Typeahead start
 // ============================================================================
 var substringMatcher = function (strs) {
@@ -46,11 +60,10 @@ $.get("/api/ingredients", function (data) {
         replaceIngredientsDisplay();
         getIngredients(ingredientSuggestion);
         var ingLI = $("<li>").text(ingredientSuggestion);
-        var ingDeleteBtn = $("<button>").addClass("ingDelete").text("Delete");
+        var ingDeleteBtn = $("<button>").addClass("ingDelete").attr("value", ingredientSuggestion).text("Delete").on("click", deleteBtn);
         $(ingLI).append(ingDeleteBtn);
         $(".added-ingredients").append(ingLI);
         $('.typeahead').typeahead('val', '');
-        drinksDisplay();
     });
 });
 // ============================================================================
@@ -60,6 +73,7 @@ $.get("/api/ingredients", function (data) {
 function getIngredients(ing) {
     $.get("/api/ingredients/" + ing, function (data) {
         ingredientsObjArray.push(data);
+        drinksDisplay();
     });
 }
 
@@ -73,36 +87,68 @@ function replaceIngredientsDisplay() {
 
 // Link typeahead and drink_controller
 function drinksDisplay() {
-    var ingredientsID = ingredientsObjArray.map((ing) => {
-        return ing.id
-    }).join('&');
-
+    var ingredientsID;
+    if (ingredientsObjArray.length === 1) {
+        ingredientsID = ingredientsObjArray[0].id
+    } else if (ingredientsObjArray.length > 1) {
+        ingredientsID = ingredientsObjArray.map((ing) => {
+            return ing.id
+        }).join('&');
+    } else {
+        displayReset();
+        return;
+    }
+    console.log(ingredientsID);
     $.get("/api/drinks/ingredient/" + ingredientsID)
         .then((data) => {
             $("#leftCard").empty();
             $("#rightCard").empty();
             data.forEach((drink) => {
-                //do some jQuery here to show the drink
-                console.log(drink);
-                if (drink.missingIngCount === 0) {
-                    // Drinks you can make
-                    // BUTTON --> drink name
-                    var drinkList = $("<button>").addClass("btn btn-link drinkList")
-                    $(drinkList).attr("data-toggle", "collapse").attr("data-target", "#drink-info" + drink.id).attr("aria-expanded", "false").attr("aria-controls", "drink-info" + drink.id);
-                    $(drinkList).text(drink.name);
-                    // DIV --> drink info
-                    var drinkInfo = $("<div>").addClass("collapse").attr("id", "drink-info" + drink.id).text(drink.name);
-                    $("#leftCard").append(drinkList, drinkInfo);
-                } else if (drink.missingIngCount < 3 && drink.missingIngCount > 0) {
-                    //Drinks you can't make yet
-                    // BUTTON --> drink name
-                    var almostDrinkList = $("<button>").addClass("btn btn-link almostDrinkList")
-                    $(almostDrinkList).attr("data-toggle", "collapse").attr("data-target", "#drink-info" + drink.id).attr("aria-expanded", "false").attr("aria-controls", "drink-info" + drink.id);
-                    $(almostDrinkList).text(drink.name);
-                    // DIV --> drink info
-                    var almostDrinkInfo = $("<div>").addClass("collapse").attr("id", "drink-info" + drink.id).text(drink.name);
-                    $("#rightCard").append(almostDrinkList, almostDrinkInfo);
-                }
+                // Getting drink card info
+                $.get("/api/drinks/" + drink.id).then((drinkCardInfo) => {
+                    //do some jQuery here to show the drink
+                    if (drink.missingIngCount === 0) {
+                        // Drinks you can make
+                        // BUTTON --> drink name
+                        var drinkList = $("<button>").addClass("btn btn-link drinkList")
+                        $(drinkList).attr("data-toggle", "collapse").attr("data-target", "#drink-info" + drink.id).attr("aria-expanded", "false").attr("aria-controls", "drink-info" + drink.id);
+                        $(drinkList).text(drink.name);
+                        // DIV --> drink info
+                        // var drinkInfo = $("<div>").addClass("collapse").attr("id", "drink-info" + drink.id).text(drink.name);
+                        var drinkInfo = cardBuilder(drinkCardInfo);
+                        $("#leftCard").append(drinkList, drinkInfo);
+                    } else if (drink.missingIngCount < 3 && drink.missingIngCount > 0) {
+                        //Drinks you can't make yet
+                        // BUTTON --> drink name
+                        var almostDrinkList = $("<button>").addClass("btn btn-link almostDrinkList")
+                        $(almostDrinkList).attr("data-toggle", "collapse").attr("data-target", "#drink-info" + drink.id).attr("aria-expanded", "false").attr("aria-controls", "drink-info" + drink.id);
+                        $(almostDrinkList).text(drink.name);
+                        // DIV --> drink info
+                        // var almostDrinkInfo = $("<div>").addClass("collapse").attr("id", "drink-info" + drink.id).text(drink.name);
+                        var almostDrinkInfo = cardBuilder(drinkCardInfo);
+                        $("#rightCard").append(almostDrinkList, almostDrinkInfo);
+                    }
+                });
             });
         });
+
+    // Drink cards
+    function cardBuilder(drinkCardInfo) {
+        var drinkInfo = $("<div>").addClass("collapse").attr("id", "drink-info" + drinkCardInfo.drinkID);
+        var contentUL = $("<ul>")
+        drinkCardInfo.contents.forEach((content) => {
+            var contentLI = $("<li>").text(content.ingridentAmount + " " + content.ingridentName);
+            $(contentUL).append(contentLI);
+        });
+        var contentInstructions = $("<div>").text(drinkCardInfo.instructions);
+        $(drinkInfo).append(contentUL, contentInstructions);
+        return drinkInfo;
+    }
+
+    // Reset Display
+    function displayReset() {
+        $("#leftCard").empty();
+        $("#rightCard").empty();
+    }
 }
+
